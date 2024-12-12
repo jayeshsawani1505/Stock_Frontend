60ca0082c4c28e29d02d761704ba6b195ab84cfd
krupalsinhchavda <krupalsinh.chavda.epistic@gmail.com>
2024-12-12 13:27:05 +0530

    env set up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // baseURL: 'https://stock1-12de6a7f0d09.herokuapp.com/api',
-  baseURL: 'http://localhost:3000/api',
-  ImageUrl: 'http://localhost:3000'
+  baseURL: 'https://stock1-12de6a7f0d09.herokuapp.com/api',
+  // baseURL: 'http://localhost:3000/api',
+  // ImageUrl: 'http://localhost:3000'
 };
